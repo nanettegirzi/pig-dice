51be21ce3ee3569e93f46b539570ff88fc899dce
Brittany Thirty, Faiza Husain and Nanette Girzi <nanette@>
2018-01-24 14:05:18 -0800

    Add player prototype for roll one

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,42 @@
 //BACK-END LOGIC
 
-//random number generator
-var counter = Math.floor(Math.random() * 7);
+var player1;
+var player2;
 
-function Player () {
-  this.current = 0;
-  this.total = 0;
+function Player() {
+  this.currentScore = 0;
+  this.totalScore = 0;
   this.dieRoll = 0;
-  this.turn = turn;
+  //this.turn = 0;
 }
 
+function dieRoll(){
+    return Math.ceil(Math.random() * 6);
+    console.log(number);
+}
 Player.prototype.roll = function () {
   if(this.dieRoll === 1) {
-    this.current = 0;
+    this.currentScore = 0;
     counter++;
   } else {
-    this.current += this.dieRoll;
+    this.currentScore += this.dieRoll;
     }
   }
   Player.prototype.hold = function() {
-    this.total += this.current;
-    this.current = 0;
+    this.totalScore += this.currentScore;
+    this.currentScore = 0;
   }
 
-  function switchPlayer(){
-    if (this.dieRoll === 1) {
-      alert("Sorry! Your turn is over.");
+  Player.prototype.rollOne = function(){
+    if(this.dieRoll === 1) {
+      this.currentScore = 0;
+      alert("You rolled a ONE! Your turn is OVER! Dundundunnnnnn!!");
     } else {
-      return false;
+      this.currentScore += this.dieRoll;
     }
   }
+
+
 
 //Front-End
 $(document).ready(function (){
@@ -37,18 +44,26 @@ $(document).ready(function (){
   var die1 = $("#randomnum1").val("counter");
 
 
-    $("#roll").click(function(){
-    var number = Math.ceil(Math.random() * 6);
-    console.log(number);
-    $("#randomnum1").text(number);
+    $("#roll1").click(function(){
+      // dieRoll();
+      // var number = Math.ceil(Math.random() * 6);
+      // console.log(number);
+      $("#roll-result1").text(dieRoll());
     });
 
-    var Player1 = new Player (this.current, this.total, this.dieRoll, this.turn);
-    var Player2 = new Player (current, total, dieRoll);
+    $("#roll2").click(function(){
+      // dieRoll();
+      // var number = Math.ceil(Math.random() * 6);
+      // console.log(number);
+      $("#roll-result2").text(dieRoll());
+    });
+
+    player1 = new Player();
+    player2 = new Player();
 
     // if (switchPlayer(this.dieRoll === 1)){
     //   $(alert("Sorry! Your turn is over")).show();
     // } else {
-    //   this.current += this.dieRoll;
+    //   this.currentScore += this.dieRoll;
     // }
 });
